40ae3740ef23e05c8ae2c0ca54415f0dccd2b671
Fernando Cordón <fcordon@pin.upv.es>
2021-03-25 23:33:31 +0100

    Fin sección 5

--- dist/routes/router.js
+++ dist/routes/router.js
@@ -4,12 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
-// 2 Lo importo de aquí
 const server_1 = __importDefault(require("../classes/server"));
-// 7 Esta comunicación entre los servicios Rest que acabamos de hacer es muy interesante  
-// Nos ayuda a que otros usuarios que no están conectados a socket puedan enviar información
-// y que los usuarios que están trabajando en tiempo real se den cuenta de eventos
-// que suceden a pesar de que no están esos usuarios conectados con Sockets
 const router = express_1.Router();
 router.get('/mensajes', (req, res) => {
     res.json({
@@ -20,16 +15,11 @@ router.get('/mensajes', (req, res) => {
 router.post('/mensajes', (req, res) => {
     const cuerpo = req.body.cuerpo;
     const de = req.body.de;
-    // 7 Creo otro payload
     const payload = {
         de,
         cuerpo
     };
-    // 5 instancia de nuestro server
     const server = server_1.default.instance;
-    // 6 Ahora le enviamos esto a todo el mundo y por eso no ponemos el in
-    // En angular estamos emitiendo los mensajes con el 'de' y el 'cuerpo'
-    // Voy a Postman: localhost:5000/mensajes y OK llega el mensaje a la web
     server.io.emit('mensaje-nuevo', payload);
     res.json({
         ok: true,
@@ -41,23 +31,11 @@ router.post('/mensajes/:id', (req, res) => {
     const cuerpo = req.body.cuerpo;
     const de = req.body.de;
     const id = req.params.id;
-    // 3 Creo los datos que voy a enviar a un usuario privado
-    // el de y el cuerpo del body del posteo
     const payload = {
         de,
         cuerpo
     };
-    // 1 Declaramos la instancia de nuestro server. Como es un Singleton tenemos la misma instancia que corre ya en node
-    // Este server tiene una propiedad que es nuestro seridor de sockets (public io: socketIO.Server;)
     const server = server_1.default.instance;
-    // 2 para mandar un mensaje, a uno o a todos los usuarios (propiedad io)
-    // El método in me sirve para mandar un mensaje a una persona que se encuentra en un canal particular
-    // Todas las personas en Socket están conectadas al chat global y a una sala que tiene como nombre el mismo id
-    // Por eso al mandar el id del usuario le envío un mensaje a ese usuario
-    // 4 Y le pongo el payload
-    // Recargo el navegador, copio el id, en postman: localhost:5000/mensajes/LHUC02PQtMrZ_sfPAAAH
-    // Y puedo ver en la consola del angular que le ha llegado el mensaje
-    // lo importante es que ya estamos recibiendo mensajes privados
     server.io.in(id).emit('mensaje-privado', payload);
     res.json({
         ok: true,
